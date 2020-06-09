1c43b9c3ca9c2fe76aa0d31a2cd86ec7dcee12f5
Katherine McHugh <katiemchugh5@gmail.com>
2020-06-08 20:34:33 -0400

    projects updates

--- js/modal.js
+++ js/modal.js
@@ -38,6 +38,46 @@ span2.onclick = function() {
   modal2.style.display = "none";
 }
 
+/* MODAL 3 */
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+/* MODAL 4 */
+// Get the modal
+var modal4 = document.getElementById("myModal4");
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("myBtn4");
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close")[3];
+
+// When the user clicks on the button, open the modal
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
 /* ALL MODALS */
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -46,5 +86,11 @@ window.onclick = function(event) {
   }
   if (event.target == modal2) {
     modal2.style.display = "none";
+  }
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+  if (event.target == modal4) {
+    modal4.style.display = "none";
   }
 }
